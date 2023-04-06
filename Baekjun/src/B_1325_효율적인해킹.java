2a739eb57703bbd64768da541415d83b19bdd4ea
Jhoyoon <junghoyoon505@gmail.com>
2023-04-06 22:24:33 +0900

    20230406

---
+++ Baekjun/src/B_1325_효율적인해킹.java
@@ -0,0 +1,124 @@
+// import java.io.*;
+// import java.util.*;
+// public class B_1325_효율적인해킹 {
+//     static ArrayList<Integer>[] arr;
+//     static boolean[] visited;
+//     static int[] believe;
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st= new StringTokenizer(br.readLine()," ");
+//         int N = Integer.parseInt(st.nextToken());
+//         int M = Integer.parseInt(st.nextToken());
+//         arr = new ArrayList[N+1];
+//         visited = new boolean[N+1];
+//         believe = new int[N+1];
+//         for(int i = 1;i<=N;i++){
+//             arr[i] = new ArrayList<>();
+//         }
+
+//         for(int i=0;i<M;i++){
+//             st = new StringTokenizer(br.readLine()," ");
+//             int s = Integer.parseInt(st.nextToken());
+//             int e = Integer.parseInt(st.nextToken());
+//             arr[s].add(e);
+//         }
+//         for(int i=1;i<=N;i++){
+//             visited = new boolean[N+1];
+//             BFS(i);
+//         }
+//         int max = 0;
+//         for(int i=1;i<=N;i++){
+//             if(believe[i]>max) max = believe[i];
+//         }
+//         List<Integer> sol = new ArrayList<>();
+//         for(int i=1;i<=N;i++){
+//             if(believe[i] == max){
+//                 sol.add(i);
+//             }
+//         }
+//         Collections.sort(sol);
+//         StringBuffer sb = new StringBuffer();
+//         for(int i: sol){
+//             sb.append(i+" ");
+//         }
+//         System.out.print(sb.toString());
+//     }
+//     private static void BFS(int n){
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(n);
+//         visited[n] = true;
+//         while(!q.isEmpty()){
+//             int now_node = q.poll();
+//             for(int i : arr[now_node]){
+//                 if(!visited[i]){
+//                     visited[i] = true;
+//                     q.add(i);
+//                     believe[i]++;
+//                 }
+//             }   
+//         }
+//     }
+// }
+
+import java.util.*;
+import java.io.*;
+
+public class B_1325_효율적인해킹{
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
+    static int[] believe;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr=  new ArrayList[N+1];
+        believe = new int[N+1];
+        for(int i=1;i<=N;i++){
+            arr[i] = new ArrayList<>();
+        }
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[s].add(e);
+        }
+        for(int i=1;i<=N;i++){
+            visited = new boolean[N+1];
+            // 여기서 believe 배열 초기화
+            BFS(i);
+        }
+        int max = 0;
+        for(int i=1;i<=N;i++){
+            if(believe[i]>max) max = believe[i];
+        }
+        List<Integer> sol = new ArrayList<>();
+        StringBuffer sb  =new StringBuffer();
+        for(int i=1;i<=N;i++){
+            if(believe[i]==max) sol.add(i); 
+        }
+        if(sol.size()==1)System.out.println(sol.get(0));
+        else{
+            Collections.sort(sol);
+            for(int i: sol){
+                sb.append(i+" ");
+            }
+        } 
+        System.out.print(sb.toString());
+    }
+    private static void BFS(int n){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        visited[n] = true;
+        while(!q.isEmpty()){
+            int now_node = q.poll();
+            for(int i : arr[now_node]){
+                if(!visited[i]){
+                    visited[i] = true;
+                    q.add(i);
+                    believe[i]++;
+                }
+            }
+        }
+    }
+}
