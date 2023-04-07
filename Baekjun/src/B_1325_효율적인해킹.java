6153546eb0f53cea2c0200462eaab2b4d98d7db5
Jhoyoon <junghoyoon505@gmail.com>
2023-04-07 21:03:37 +0900

    20230407

--- Baekjun/src/B_1325_효율적인해킹.java
+++ Baekjun/src/B_1325_효율적인해킹.java
@@ -1,64 +1,4 @@
-// import java.io.*;
-// import java.util.*;
-// public class B_1325_효율적인해킹 {
-//     static ArrayList<Integer>[] arr;
-//     static boolean[] visited;
-//     static int[] believe;
-//     public static void main(String[] args) throws IOException{
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         StringTokenizer st= new StringTokenizer(br.readLine()," ");
-//         int N = Integer.parseInt(st.nextToken());
-//         int M = Integer.parseInt(st.nextToken());
-//         arr = new ArrayList[N+1];
-//         visited = new boolean[N+1];
-//         believe = new int[N+1];
-//         for(int i = 1;i<=N;i++){
-//             arr[i] = new ArrayList<>();
-//         }
 
-//         for(int i=0;i<M;i++){
-//             st = new StringTokenizer(br.readLine()," ");
-//             int s = Integer.parseInt(st.nextToken());
-//             int e = Integer.parseInt(st.nextToken());
-//             arr[s].add(e);
-//         }
-//         for(int i=1;i<=N;i++){
-//             visited = new boolean[N+1];
-//             BFS(i);
-//         }
-//         int max = 0;
-//         for(int i=1;i<=N;i++){
-//             if(believe[i]>max) max = believe[i];
-//         }
-//         List<Integer> sol = new ArrayList<>();
-//         for(int i=1;i<=N;i++){
-//             if(believe[i] == max){
-//                 sol.add(i);
-//             }
-//         }
-//         Collections.sort(sol);
-//         StringBuffer sb = new StringBuffer();
-//         for(int i: sol){
-//             sb.append(i+" ");
-//         }
-//         System.out.print(sb.toString());
-//     }
-//     private static void BFS(int n){
-//         Queue<Integer> q = new LinkedList<>();
-//         q.add(n);
-//         visited[n] = true;
-//         while(!q.isEmpty()){
-//             int now_node = q.poll();
-//             for(int i : arr[now_node]){
-//                 if(!visited[i]){
-//                     visited[i] = true;
-//                     q.add(i);
-//                     believe[i]++;
-//                 }
-//             }   
-//         }
-//     }
-// }
 
 import java.util.*;
 import java.io.*;
@@ -68,11 +8,11 @@ public class B_1325_효율적인해킹{
     static boolean[] visited;
     static int[] believe;
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        arr=  new ArrayList[N+1];
+        arr = new ArrayList[N+1];
         believe = new int[N+1];
         for(int i=1;i<=N;i++){
             arr[i] = new ArrayList<>();
@@ -83,9 +23,9 @@ public class B_1325_효율적인해킹{
             int e = Integer.parseInt(st.nextToken());
             arr[s].add(e);
         }
+        // 여기서 believe 배열을 업데이트
         for(int i=1;i<=N;i++){
-            visited = new boolean[N+1];
-            // 여기서 believe 배열 초기화
+            visited=  new boolean[N+1];
             BFS(i);
         }
         int max = 0;
@@ -95,17 +35,18 @@ public class B_1325_효율적인해킹{
         List<Integer> sol = new ArrayList<>();
         StringBuffer sb  =new StringBuffer();
         for(int i=1;i<=N;i++){
-            if(believe[i]==max) sol.add(i); 
+            if(max == believe[i]) sol.add(i);
         }
-        if(sol.size()==1)System.out.println(sol.get(0));
+        if(sol.size()==1) sb.append(sol.get(0));
         else{
             Collections.sort(sol);
-            for(int i: sol){
+            for(int i : sol){
                 sb.append(i+" ");
             }
-        } 
+        }
         System.out.print(sb.toString());
     }
+
     private static void BFS(int n){
         Queue<Integer> q = new LinkedList<>();
         q.add(n);
--- Baekjun/src/B_1707_이분그래프.java
+++ Baekjun/src/B_1707_이분그래프.java
@@ -50,6 +50,65 @@
 //     }
 // }
 
+// import java.io.*;
+// import java.util.*;
+
+// public class B_1707_이분그래프{
+//     static ArrayList<Integer>[] arr;
+//     static boolean[] visited;
+//     static int[] check;
+//     static boolean go;
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int T = Integer.parseInt(br.readLine());
+//         for(int j = 0;j<T;j++){
+//             StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//             int N = Integer.parseInt(st.nextToken());
+//             int M = Integer.parseInt(st.nextToken());
+//             arr = new ArrayList[N+1];
+//             for(int i=1;i<=N;i++){
+//                 arr[i] = new ArrayList<>();
+//             }
+//             for(int i=0;i<M;i++){
+//                 st = new StringTokenizer(br.readLine(), " ");
+//                 int s = Integer.parseInt(st.nextToken());
+//                 int e = Integer.parseInt(st.nextToken());
+//                 arr[s].add(e);
+//                 arr[e].add(s);
+//             }
+//             // 아 그러네 안에서 초기화 하지 않으면 그냥 바로 바로 이분 그래프인지 유무를 확인할수 있구나!
+//             // 지금 이 문제의 목적은 check 배열을 채우는거니까!
+//             visited = new boolean[N+1];
+//             check = new int[N+1];
+//             go = true;
+//             for(int i=1;i<=N;i++){
+//                 if(go){
+//                     DFS(i);
+//                 }else{
+//                     break;
+//                 }
+//             }
+//             if(go){
+//                 System.out.println("YES");
+//             }else{
+//                 System.out.println("NO");
+//             }
+//         }
+//     }
+//     private static void DFS(int n){
+//         visited[n]=true;
+//         for(int i : arr[n]){
+//             if(!visited[i]){
+//                 visited[i] = true;
+//                 check[i] = (check[n]+1)%2;
+//                 DFS(i);
+//             }else{
+//                 if(check[i]==check[n]) go=false;
+//             }
+//         }
+//     }
+// }
+
 import java.io.*;
 import java.util.*;
 
@@ -57,53 +116,55 @@ public class B_1707_이분그래프{
     static ArrayList<Integer>[] arr;
     static boolean[] visited;
     static int[] check;
-    static boolean go;
-    public static void main(String[] args) throws IOException{
+    static boolean com;
+    public static void main(String[] args) throws IOException{ 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        for(int j = 0;j<T;j++){
+        StringBuffer sb = new StringBuffer();
+        for(int j=0;j<T;j++){
             StringTokenizer st = new StringTokenizer(br.readLine()," ");
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
             arr = new ArrayList[N+1];
+            check = new int[N+1];
+            visited = new boolean[N+1];
+            com = true;
             for(int i=1;i<=N;i++){
                 arr[i] = new ArrayList<>();
             }
+
             for(int i=0;i<M;i++){
-                st = new StringTokenizer(br.readLine(), " ");
+                st = new StringTokenizer(br.readLine()," ");
                 int s = Integer.parseInt(st.nextToken());
                 int e = Integer.parseInt(st.nextToken());
                 arr[s].add(e);
                 arr[e].add(s);
             }
-            // 아 그러네 안에서 초기화 하지 않으면 그냥 바로 바로 이분 그래프인지 유무를 확인할수 있구나!
-            // 지금 이 문제의 목적은 check 배열을 채우는거니까!
-            visited = new boolean[N+1];
-            check = new int[N+1];
-            go = true;
             for(int i=1;i<=N;i++){
-                if(go){
+                if(com){
                     DFS(i);
                 }else{
                     break;
                 }
             }
-            if(go){
-                System.out.println("YES");
+            
+            if(com){
+                sb.append("YES\n");
             }else{
-                System.out.println("NO");
+                sb.append("NO\n");
             }
         }
+        System.out.print(sb.toString());
     }
     private static void DFS(int n){
-        visited[n]=true;
+        visited[n] = true;
         for(int i : arr[n]){
             if(!visited[i]){
-                visited[i] = true;
-                check[i] = (check[n]+1)%2;
-                DFS(i);
+               visited[i] = true;
+               check[i]  = (check[n]+1)%2;
+               DFS(i);
             }else{
-                if(check[i]==check[n]) go=false;
+                if(check[i]==check[n]) com = false;
             }
         }
     }
