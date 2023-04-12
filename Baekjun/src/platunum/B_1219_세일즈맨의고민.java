12e90718196da978cac57c9a7cd249dcf1712563
Jhoyoon <junghoyoon505@gmail.com>
2023-04-12 23:07:09 +0900

    세일즈맨

--- Baekjun/src/platunum/B_1219_세일즈맨의고민.java
+++ Baekjun/src/platunum/B_1219_세일즈맨의고민.java
@@ -1,8 +1,9 @@
 package platunum;
 
+
 import java.util.*;
 import java.io.*;
-public class B_1219_세일즈맨의고민 {
+public class B_1219_세일즈맨의고민{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
@@ -10,51 +11,47 @@ public class B_1219_세일즈맨의고민 {
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        Node1219[] edges = new Node1219[M];
-        long[] citymoney = new long[N];
-        long[] sol = new long[N];
-        Arrays.fill(sol,Long.MIN_VALUE);
+        Edge1219[] edges = new Edge1219[M];
         for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine()," ");
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            edges[i] = new Node1219(s, e, t);
+            int m = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge1219(s, e, m);
         }
+        long[] money = new long[N];
         st = new StringTokenizer(br.readLine()," ");
         for(int i=0;i<N;i++){
-            citymoney[i] = Long.parseLong(st.nextToken());
+            money[i] = Long.parseLong(st.nextToken());
         }
-        sol[start] = citymoney[start];
-        for(int i=1;i<N;i++){
+        long[] sol = new long[N];
+        Arrays.fill(sol,Long.MIN_VALUE);
+        sol[start] = money[start];
+        for(int i=0;i<=N+100;i++){
             for(int j=0;j<M;j++){
-                Node1219 now = edges[j];
-                if(sol[now.start] != Integer.MAX_VALUE && sol[now.end]<sol[now.start]+citymoney[now.end]-now.money) sol[now.end] = sol[now.start]+citymoney[now.end]-now.money;
+                // 엣지 리스트를 싸그리 돌면서 조건에 해당하면 엣지 리스트를 업데이트 해준다.
+                Edge1219 now = edges[j];
+                if(sol[now.start] == Long.MIN_VALUE) continue;
+                else if(sol[now.start] == Long.MAX_VALUE) sol[now.end] = Long.MAX_VALUE;
+                else if(sol[now.end] < sol[now.start]+money[now.end]-now.price){
+                    sol[now.end] = sol[now.start]+money[now.end]-now.price;
+                    if(i>= N-1) sol[now.end] = Long.MAX_VALUE;
+                }
             }
         }
-        boolean sw = false;
-        for(int i=0;i<M;i++){
-            Node1219 now = edges[i];
-                if(sol[now.start] != Integer.MAX_VALUE && sol[now.end]<sol[now.start]+citymoney[now.end]-now.money) sw = true;
-        }
-        if(!sw){
-            if(sol[end] != Integer.MAX_VALUE) System.out.println(sol[end]);
-            else System.out.println("gg");
-            
-        }else{
-            System.out.println("Gee");
-        }
-
+        if(sol[end]==Long.MIN_VALUE) System.out.println("gg");
+        else if(sol[end] == Long.MAX_VALUE) System.out.println("Gee");
+        else System.out.println(sol[end]);
     }
 }
 
-class Node1219{
+class Edge1219{
     int start;
     int end;
-    int money;
-    Node1219(int start,int end,int money){
+    int price;
+    Edge1219(int start,int end,int price){
         this.start = start;
         this.end = end;
-        this.money = money;
+        this.price = price;
     }
 }
