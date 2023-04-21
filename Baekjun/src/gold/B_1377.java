e26677b47a8978a794e5f94d872d243d62d9c026
Jhoyoon <junghoyoon505@gmail.com>
2023-04-21 21:49:45 +0900

    22

--- Baekjun/src/gold/B_1377.java
+++ Baekjun/src/gold/B_1377.java
@@ -1,75 +1,37 @@
-package gold;
-// import java.io.BufferedReader;
-// import java.io.InputStreamReader;
-// import java.nio.BufferOverflowException;
+// package gold;
+
+
 // import java.util.*;
 // import java.io.*;
 
-// public class B_1377 {
-//     public static void main(String[] args) throws IOException{
-//       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//       int N = Integer.parseInt(br.readLine());
-//       Node[] arr = new Node[N];
-//       for(int i=0;i<N;i++){
-//         arr[i] = new Node(i,Integer.parseInt(br.readLine()));
-//       }
-//       Arrays.sort(arr);
-//       int max = 0;
-//       for(int i=0;i<N;i++){
-//         if(arr[i].index - i>max) max = arr[i].index-i;
-//       }
-      
-//       System.out.println(max+1);
+// public class B_1377{
+//   public static void main(String[] args) throws IOException{
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     int n = Integer.parseInt(br.readLine());
+//     Node[] arr = new Node[n];
+//     for(int i=0;i<n;i++){
+//       arr[i] = new Node(i,Integer.parseInt(br.readLine()));
+//     }
+//     Arrays.sort(arr,(o1,o2)->{
+//       return o1.value-o2.value;
+//     });
+//     int max = 0;
+//     for(int i=0;i<n;i++){
+//       if(arr[i].index-i>max) max = arr[i].index-i;
+//     }
+//     System.out.println(max+1);
 
-//   }    
+
+
+
+//   }
 // }
 
-// class Node implements Comparable<Node>{
-//   public int index;
-//   public int value;
-
+// class Node{
+//   int index;
+//   int value;
 //   Node(int index,int value){
-//     super();
 //     this.index = index;
 //     this.value = value;
 //   }
-  
-//   public int compareTo(Node o){
-//     return this.value - o.value;
-//   }
 // }
-
-import java.util.*;
-import java.io.*;
-
-public class B_1377{
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    Node[] arr = new Node[n];
-    for(int i=0;i<n;i++){
-      arr[i] = new Node(i,Integer.parseInt(br.readLine()));
-    }
-    Arrays.sort(arr,(o1,o2)->{
-      return o1.value-o2.value;
-    });
-    int max = 0;
-    for(int i=0;i<n;i++){
-      if(arr[i].index-i>max) max = arr[i].index-i;
-    }
-    System.out.println(max+1);
-
-
-
-
-  }
-}
-
-class Node{
-  int index;
-  int value;
-  Node(int index,int value){
-    this.index = index;
-    this.value = value;
-  }
-}
--- Baekjun/src/gold/B_1722_순열의순서.java
+++ Baekjun/src/gold/B_1722_순열의순서.java
@@ -1,61 +1,99 @@
 package gold;
+// import java.io.*;
+// import java.util.*;
+// public class B_1722_순열의순서{
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+//         int N = Integer.parseInt(br.readLine());
+//         long[] F = new long[21];
+//         int[] S = new int[21];
+//         boolean[] visited = new boolean[21];
+//         F[0] = 1;
+//         for(int i=1;i<=20;i++){
+//             F[i] = F[i-1]*i;
+//         }
+//         StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//         int Q = Integer.parseInt(st.nextToken());
+//         if(Q==1){
+//             long K = Long.parseLong(st.nextToken());
+//             for(int i=1;i<=N;i++){
+//                 for(int j=1,cnt=1;j<=N;j++){
+//                     if(visited[j]) continue;
+//                     if(K<=cnt*F[N-i]){
+//                         visited[j] = true;
+//                         K = K-((cnt-1)*F[N-i]);
+//                         S[i]=j;
+//                         break;
+//                     }
+//                     cnt++;
+//                 }
+//             }
+//             StringBuffer sb = new StringBuffer();
+//             for(int i=1;i<=N;i++){
+//                 sb.append(S[i]+" ");
+//             }
+//             System.out.print(sb.toString());
+//         }else if(Q==2){
+//             long K = 1;
+//             for(int i=1;i<=N;i++){
+//                 long cnt = 0;
+//                 S[i] = Integer.parseInt(st.nextToken());
+//                 for(int j=1;j<S[i];j++){
+//                     if(!visited[j]) cnt++;
+//                 }
+//                 K = K+cnt*F[N-i];
+//                 visited[S[i]] = true;
+//             }
+//             System.out.println(K);
+//         }
+//     }
+// }
 import java.io.*;
 import java.util.*;
-public class B_1722_순열의순서 {
-    public static void main(String[] args) throws IOException{
-        int N,Q;
-        // 각 자리별로 만들수 있는 경우의 수 저장하기 그럼 이 안에 모든 팩토리얼을 담아야겠네?아 그래서 long이구나
-        long F[] = new long[21];
-        // 순열을 담는 배열 숫자의 배열을 담는다
-        int S[] = new int[21];
-        // 숫자 사용 유무 저장 배열
-        boolean visited[] = new boolean[21];
+public class B_1722_순열의순서{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //숫자의 개수를 입력받음
-        N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        // 아 질문이 뭔지
-        Q = Integer.parseInt(st.nextToken());
-        // 0팩은 무조건 1.계산이 의미가 없으므로 그냥 1로 초기화해줌
+        int N = Integer.parseInt(br.readLine());
+        long[] F = new long[21];
+        int[] S = new int[21];
+        boolean[] visited = new boolean[21];
         F[0] = 1;
-        // 모든 팩토리얼을 넣어준다. f[0]은 구했으니까 1부터 N까지 구해준다.
         for(int i=1;i<=N;i++){
             F[i] = F[i-1]*i;
         }
-        // 1번문제. 3번째로 정렬된 팩토리얼을 찾아라!
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int Q = Integer.parseInt(st.nextToken());
         if(Q == 1){
-            // K번째로 정렬된 수열을 찾아야한다.
             long K = Long.parseLong(st.nextToken());
-            // 0번째는 없으므로 1부터 돌림
             for(int i=1;i<=N;i++){
-                for(int j=1,cnt = 1;j<=N;j++){
+                for(int j=1,cnt=1;j<=N;j++){
                     if(visited[j]) continue;
-                    if(K<=cnt*F[N-i]){
-                        K-=((cnt-1)*F[N-i]);
-                        S[i]=j;
-                        visited[j]=true;
+                    if(K<=cnt * F[N-i]){
+                        visited[j] = true;
+                        K = K-(cnt-1) * F[N-i];
+                        S[i] = j;
                         break;
                     }
                     cnt++;
                 }
             }
+            StringBuffer sb = new StringBuffer();
             for(int i=1;i<=N;i++){
-                System.out.print(S[i]+" ");
+                sb.append(S[i]+" ");
             }
-        }else{
+            System.out.print(sb.toString());
+        }else if(Q==2){
             long K = 1;
             for(int i=1;i<=N;i++){
-                S[i]=Integer.parseInt(st.nextToken());
+                S[i] = Integer.parseInt(st.nextToken());
                 long cnt = 0;
                 for(int j=1;j<S[i];j++){
-                    if(visited[j]==false){
-                        cnt++;
-                    }
+                    if(!visited[j]) cnt++;
                 }
-                K+=cnt*F[N-i];
                 visited[S[i]]=true;
+                K = K + cnt*F[N-i];
             }
-            System.out.println(K);
+            System.out.print(K);
         }
     }
 }
