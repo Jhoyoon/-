180e6956cd9adef2cc87f1bc37a5a20b6f4d15ca
Jhoyoon <junghoyoon505@gmail.com>
2023-04-23 21:49:48 +0900

    22

--- Baekjun/src/silver/B_1463_1로만들기.java
+++ Baekjun/src/silver/B_1463_1로만들기.java
@@ -1,16 +1,19 @@
 package silver;
 import java.io.*;
-public class B_1463_1로만들기 {
+import java.util.*;
+public class B_1463_1로만들기{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N+1];
-        arr[1]=0;
-        for(int i=2;i<=N;i++){
-            arr[i] = arr[i-1]+1;
-            if(arr[i]%3==0) arr[i]=Math.min(arr[i/3]+1,arr[i]);
-            if(arr[i]%2==0) arr[i] = Math.min(arr[i],arr[i/2]+1);
+        // 인덱스 숫자가 1이 되는데 필요한 연산 횟수
+        int[] D = new int[N+2];
+        D[1] = 0;
+        D[2] = 1;
+        for(int i=3;i<=N;i++){
+            D[i] = D[i-1]+1;
+            if(i%3==0) D[i] = Math.min(D[i/3]+1,D[i]);
+            if(i%2==0) D[i] = Math.min(D[i/2]+1,D[i]);
         }
-        System.out.print(arr[N]);
+        System.out.print(D[N]);
     }
 }
