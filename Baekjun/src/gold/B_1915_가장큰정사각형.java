f211a8d69174f1fa9a920e36ff758e4e3a6403b4
Jhoyoon <junghoyoon505@gmail.com>
2023-04-25 18:49:29 +0900

    dp

--- Baekjun/src/gold/B_1915_가장큰정사각형.java
+++ Baekjun/src/gold/B_1915_가장큰정사각형.java
@@ -1,19 +1,47 @@
 package gold;
+// import java.io.*;
+// import java.util.*;
+// public class B_1915_가장큰정사각형 {
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//         int N = Integer.parseInt(st.nextToken());
+//         int M = Integer.parseInt(st.nextToken());
+//         long[][] D = new long[1001][1001];
+//         long max = 0;
+//         for(int i=1;i<=N;i++){
+//             String s = br.readLine();
+//             for(int j=1;j<=M;j++){
+//                 D[i][j] = s.charAt(j-1)-'0';
+//                 if(D[i][j] == 1 && j>1 && i>1){
+//                     D[i][j] = Math.min(D[i-1][j-1],Math.min(D[i-1][j],D[i][j-1]))+D[i][j];
+//                 }
+//                 if(max<D[i][j]) max = D[i][j];
+//             }
+//         }
+//         System.out.println(max*max);       
+//     }
+// }
 import java.io.*;
 import java.util.*;
-public class B_1915_가장큰정사각형 {
+public class B_1915_가장큰정사각형{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N+1][M+1];
+        long[][] D = new long[1001][1001];
+        long max = 0;
         for(int i=1;i<=N;i++){
-            st = new StringTokenizer(br.readLine()," ");
+            String s = br.readLine();
             for(int j=1;j<=M;j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                D[i][j] = s.charAt(j-1)-'0';
+                if(D[i][j]== 1 &&  i>1 && j>1){
+                    D[i][j] = Math.min(D[i-1][j-1],Math.min(D[i-1][j],D[i][j-1]))+D[i][j];
+                }
+                if(max < D[i][j]) max = D[i][j];
             }
         }
-        
+        System.out.print(max*max);
     }
 }
