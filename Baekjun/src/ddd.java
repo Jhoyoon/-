bdbb4ba328c99512f86548385c0e7ab17e0949da
Jhoyoon <junghoyoon505@gmail.com>
2023-05-03 20:48:49 +0900

    55

--- Baekjun/src/ddd.java
+++ Baekjun/src/ddd.java
@@ -1,37 +1,14 @@
-import java.util.*;
 import java.io.*;
-import java.time.*;
-
-
-public class ddd {
-	public static void main(String args[]) throws IOException {
+import java.util.*;
+public class ddd{
+  public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
-    int[] arr  = new int[N];
-    StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    for(int i=0;i<N;i++){
-      arr[i] = Integer.parseInt(st.nextToken());
-    }
-    Arrays.sort(arr);
-    int count=0;
-    for(int i=0;i<N;i++){
-     int start = 0;
-     int finish = N-1;
-    int k = arr[i];
-    while(finish>start){
-      if(arr[finish]+arr[start]>k){
-        finish--;
-      }else if(arr[finish]+arr[start]<k){
-        start++;
-      }else{
-        if(start!=i && finish!=i){
-          count++;
-          break;
-        }else if(start == i) start++;
-        else if(finish==i) finish--;
+    for(int i=1;i<=N;i++){
+      for(int j=1;j<=N;j++){
+        if(i <= j) System.out.print("*");
       }
+      System.out.println();
     }
-   }
-   System.out.println(count);
   }
 }
--- Baekjun/src/gold/B_17298_오큰수.java
+++ Baekjun/src/gold/B_17298_오큰수.java
@@ -1,30 +1,30 @@
 package gold;
 import java.io.*;
 import java.util.*;
-public class B_17298_오큰수 {
+public class B_17298_오큰수{
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] input = new int[N+1];
-        int[] answer = new int[N+1];
+        int[] ans = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         for(int i=1;i<=N;i++){
             input[i] = Integer.parseInt(st.nextToken());
         }
-        Stack<Integer> myst = new Stack<>();
-        myst.push(1);
+        Stack<Integer> myStack = new Stack<>();
+        myStack.add(1);
         for(int i=2;i<=N;i++){
-            while(!myst.isEmpty() && input[myst.peek()] <input[i]){
-                answer[myst.pop()] = input[i];
+            while(!myStack.isEmpty() && input[myStack.peek()]<input[i]){
+                ans[myStack.pop()] = input[i];
             }
-            myst.push(i);
+            myStack.add(i);
         }
-        while(!myst.isEmpty()){
-            answer[myst.pop()] = -1;
+        while(!myStack.isEmpty()){
+            ans[myStack.pop()] = -1;
         }
         StringBuffer sb = new StringBuffer();
         for(int i=1;i<=N;i++){
-            sb.append(answer[i]+" ");
+            sb.append(ans[i]+" ");
         }
         System.out.println(sb.toString());
     }
