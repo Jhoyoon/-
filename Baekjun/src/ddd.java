6d58a52965f38e0e709368f507cbbcc375b970cd
Jhoyoon <junghoyoon505@gmail.com>
2023-05-05 15:34:05 +0900

    ddd

--- Baekjun/src/ddd.java
+++ Baekjun/src/ddd.java
@@ -2,16 +2,6 @@ import java.io.*;
 import java.util.*;
 public class ddd{
   public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    int N = st.countTokens();
-    int[] a = new int[N];
-    for(int i=0;i<N;i++){
-      a[i] = Integer.parseInt(st.nextToken());
-    }
-    Arrays.sort(a);
-    for(int i : a){
-      System.out.print(i+" ");
-    }
+    Map<String,String> map = new HashMap<>();
   }
 }
---
+++ Baekjun/src/silver/B_1764_듣보잡.java
@@ -0,0 +1,30 @@
+package silver;
+import java.io.*;
+import java.util.*;
+
+public class B_1764_듣보잡 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i<N;i++){
+            set.add(br.readLine());
+        }
+        StringBuffer sb = new StringBuffer();
+        ArrayList<String> ans = new ArrayList<>();
+        for(int i=0;i<M;i++){
+            String tmp = br.readLine();
+            if(set.contains(tmp)){
+                ans.add(tmp);
+            } 
+        }
+        Collections.sort(ans);
+        sb.append(ans.size()+"\n");
+        for(String i : ans){
+            sb.append(i+"\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
