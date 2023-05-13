92c8e39bcb731c2172a9c1e9a4651d573125ac61
Jhoyoon <junghoyoon505@gmail.com>
2023-05-13 19:54:44 +0900

    물통은 어렵다

--- Baekjun/src/gold/B_2251_물통.java
+++ Baekjun/src/gold/B_2251_물통.java
@@ -1,168 +1,48 @@
 package gold;
-// import java.util.*;
-// import java.io.*;
-
-// public class B_2251_물통{
-//     static int[] now;
-//     static boolean[][] visited;
-//     static boolean[] answer;
-//     static int[] receiver = {0,0,1,1,2,2};
-//     static int[] sender = {2,2,0,0,1,1};
-//     public static void main(String[] args) throws IOException{
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         StringTokenizer st=  new StringTokenizer(br.readLine()," ");
-//         now = new int[3];
-//         visited = new boolean[201][201];
-//         answer = new boolean[201];
-//         now[0] = Integer.parseInt(st.nextToken());
-//         now[1] = Integer.parseInt(st.nextToken());
-//         now[2] = Integer.parseInt(st.nextToken());
-//         BFS();
-//         StringBuffer sb  =new StringBuffer();
-//         for(int i=0;i<201;i++){
-//             if(answer[i]) sb.append(i+" ");
-//         }
-//         System.out.print(sb.toString());
-//     }
-//     private static void BFS(){
-//         Queue<int[]> q = new LinkedList<>();
-//         q.add(new int[]{0,0});
-//         visited[0][0] = true;
-//         answer[now[2]] = true;
-//         while(!q.isEmpty()){
-//             int[] p = q.poll();
-//             int A = p[0];
-//             int B = p[1];
-//             int C = now[2]-A-B;
-//             for(int i=0;i<6;i++){
-//                 int[] next = {A,B,C};
-//                 next[receiver[i]]=next[receiver[i]]+next[sender[i]];
-//                 next[sender[i]]=0;
-//                 if(next[receiver[i]]>now[receiver[i]]){
-//                     next[sender[i]] = next[receiver[i]]-now[receiver[i]]; 
-//                     next[receiver[i]] = now[receiver[i]];
-//                 }
-//                 if(!visited[next[0]][next[1]]){
-//                     q.add(new int[]{next[0],next[1]});
-//                     visited[next[0]][next[1]]=true;
-//                     if(next[0]==0) answer[next[2]] = true;
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// import java.util.*;
-// import java.io.*;
-
-// public class B_2251_물통{
-//     static boolean[][] visited;
-//     static boolean[] check;
-//     static int[] receiver={0,0,1,1,2,2};
-//     static int[] sender = {2,2,0,0,1,1};
-//     static int[] now;
-//     public static void main(String[] args) throws IOException{
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-//         now = new int[3];
-//         check = new boolean[201];
-//         visited = new boolean[201][201];
-//         now[0]  = Integer.parseInt(st.nextToken());
-//         now[1] = Integer.parseInt(st.nextToken());
-//         now[2] = Integer.parseInt(st.nextToken());
-//         BFS();
-//         StringBuffer sb = new StringBuffer();
-//         for(int i=0;i<check.length;i++){
-//             if(check[i]) sb.append(i+" ");
-//         }
-//         System.out.print(sb.toString());
-//     }
-//     private static void BFS(){
-//         Queue<AB> q = new LinkedList<>();
-//         q.add(new AB(0,0));
-//         visited[0][0] = true;
-//         check[now[2]] = true;
-//         while(!q.isEmpty()){
-//             AB p = q.poll();
-//             int A = p.A;
-//             int B = p.B;
-//             int C = now[2]-A-B;
-//            for(int i=0;i<6;i++){
-//             int[] next = {A,B,C};
-//             next[receiver[i]] += next[sender[i]];
-//             next[sender[i]] = 0;
-//             if(next[receiver[i]]>now[receiver[i]]){
-//                 next[sender[i]] = next[receiver[i]] - now[receiver[i]];
-//                 next[receiver[i]] = now[receiver[i]];
-//             }
-//             if(!visited[next[0]][next[1]]){
-//                 visited[next[0]][next[1]] = true;
-//                 q.add(new AB(next[0],next[1]));
-//                 if(next[0]==0) check[next[2]] = true;
-//             }
-//            }
-//         }
-
-//     }
-// }
-
-// class AB{
-//     int A;
-//     int B;
-//     AB(int A,int B){
-//         this.A = A;
-//         this.B = B;
-//     }
-// }
-
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class B_2251_물통{
     static int[] sender = {0,0,1,1,2,2};
-    static int[] receiver = {2,2,0,0,1,1};
-    static int[] now;
-    static boolean[] answer;
+    static int[] receiver = {1,1,2,2,0,0};
     static boolean[][] visited;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        now = new int[3];
-        answer = new boolean[201];
+    static boolean[] answer;
+    static int[] now;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int now[] = new int[3];
+        now[0] = Integer.parseInt(st.nextToken());
+        now[1] = Integer.parseInt(st.nextToken());
+        now[2] = Integer.parseInt(st.nextToken());
         visited = new boolean[201][201];
-        now[0] = sc.nextInt();
-        now[1] = sc.nextInt();
-        now[2] = sc.nextInt();
-        
+        answer = new boolean[201];
         BFS();
-        StringBuffer sb = new StringBuffer();
-       for(int i=0;i<201;i++){
-        if(answer[i]) sb.append(i+" ");
-       }
-        System.out.print(sb.toString());
+        for(int i=0;i<answer.length;i++){
+            if(answer[i]) System.out.println(i+" ");
+        }
     }
     private static void BFS(){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {0,0});
+        Queue<AB> q = new LinkedList<>();
+        q.add(new AB(0,0));
+        visited[0][0] = true;
         answer[now[2]] = true;
         while(!q.isEmpty()){
-            int[] p = q.poll();
-            int A = p[0];
-            int B = p[1];
+            AB p = q.poll();
+            int A = p.A;
+            int B = p.B;
             int C = now[2]-A-B;
-            for(int i=0;i<6;i++){
+            for(int k=0;k<6;k++){
                 int[] next = {A,B,C};
-                next[receiver[i]] = next[receiver[i]]+next[sender[i]];
-                next[sender[i]] = 0;
-                if(next[receiver[i]]>now[receiver[i]]){
-                    next[sender[i]] = next[receiver[i]]-now[receiver[i]];
-                    next[receiver[i]] = now[receiver[i]];
-                }
-                if(!visited[next[0]][next[1]]){
-                    visited[next[0]][next[1]] = true;
-                    q.add(new int[]{next[0],next[1]});
-                    if(next[0]==0) answer[next[2]] = true;
-                }
+                next[receiver[k]] += next
             }
+        }
+    }
+    static class AB{
+        int A;
+        int B;
+        AB(int A,int B){
+            this.A = A;
+            this.B = B;
         }
     }
 }
--- Baekjun/src/silver/B_1325_효율적인해킹.java
+++ Baekjun/src/silver/B_1325_효율적인해킹.java
@@ -1,64 +1,107 @@
 package silver;
-
-
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
+// ** 내 방식대로 푼 코드 **
+// public class B_1325_효율적인해킹{
+//     static ArrayList<Integer>[] arr;
+//     static boolean[] visited;
+//     static int[] hack;
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//         int node = Integer.parseInt(st.nextToken());
+//         int edge = Integer.parseInt(st.nextToken());
+//         arr = new ArrayList[node+1];
+//         visited = new boolean[node+1];
+//         hack = new int[node+1];
+//         for(int i=1;i<=node;i++){
+//             arr[i] = new ArrayList<>();
+//         }
+//         for(int i=0;i<edge;i++){
+//             st = new StringTokenizer(br.readLine()," ");
+//             int s = Integer.parseInt(st.nextToken());
+//             int e = Integer.parseInt(st.nextToken());
+//             arr[e].add(s);
+//         }
+//         for(int i=1;i<=node;i++){
+//             BFS(i);
+//             visited = new boolean[node+1];
+//         }
+//         int max = 0;
+//         for(int i : hack){
+//             if(i > max) max = i;
+//         }
+//         StringBuffer sb = new StringBuffer();
+//         for(int i=1;i<=node;i++){
+//             if(hack[i] == max) sb.append(i+" ");
+//         }
+//         System.out.println(sb.toString());
+//     }
+//     private static void BFS(int node){
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(node);
+//         visited[node] = true;
+//         int num = 1;
+//         while(!q.isEmpty()){
+//             int now = q.poll();
+//             for(int i : arr[now]){
+//                 if(!visited[i]){
+//                     q.add(i);
+//                     visited[i] = true;
+//                     num = num+1;
+//                 }
+//             }
+//         }
+//         hack[node] = num;
+//     }
+// }
+// ** 책에 나와있는 코드
 public class B_1325_효율적인해킹{
     static ArrayList<Integer>[] arr;
     static boolean[] visited;
-    static int[] believe;
+    static int[] hack;
     public static void main(String[] args) throws IOException{
-        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        arr = new ArrayList[N+1];
-        believe = new int[N+1];
-        for(int i=1;i<=N;i++){
+        int node = Integer.parseInt(st.nextToken());
+        int edge = Integer.parseInt(st.nextToken());
+        arr = new ArrayList[node+1];
+        for(int i=1;i<=node;i++){
             arr[i] = new ArrayList<>();
         }
-        for(int i=0;i<M;i++){
+        visited = new boolean[node+1];
+        hack = new int[node+1];
+        for(int i=0;i<edge;i++){
             st = new StringTokenizer(br.readLine()," ");
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             arr[s].add(e);
         }
-        // 여기서 believe 배열을 업데이트
-        for(int i=1;i<=N;i++){
-            visited=  new boolean[N+1];
+        for(int i=1;i<=node;i++){
             BFS(i);
+            visited = new boolean[node+1];
         }
         int max = 0;
-        for(int i=1;i<=N;i++){
-            if(believe[i]>max) max = believe[i];
+        for(int i : hack){
+            if(i > max) max = i;
         }
-        List<Integer> sol = new ArrayList<>();
-        StringBuffer sb  =new StringBuffer();
-        for(int i=1;i<=N;i++){
-            if(max == believe[i]) sol.add(i);
+        StringBuffer sb = new StringBuffer();
+        for(int i=1;i<=node;i++){
+            if(hack[i] == max) sb.append(i+" ");
         }
-        if(sol.size()==1) sb.append(sol.get(0));
-        else{
-            Collections.sort(sol);
-            for(int i : sol){
-                sb.append(i+" ");
-            }
-        }
-        System.out.print(sb.toString());
+        System.out.println(sb.toString());
     }
-
-    private static void BFS(int n){
+    private static void BFS(int node){
         Queue<Integer> q = new LinkedList<>();
-        q.add(n);
-        visited[n] = true;
+        q.add(node);
+        visited[node] = true;
         while(!q.isEmpty()){
-            int now_node = q.poll();
-            for(int i : arr[now_node]){
+            int now = q.poll();
+            for(int i : arr[now]){
                 if(!visited[i]){
-                    visited[i] = true;
                     q.add(i);
-                    believe[i]++;
+                    visited[i] = true;
+                    hack[i]++;
                 }
             }
         }
