0653c156ffc4b5e0fa7252aafa6addf7478d15d0
Jhoyoon <junghoyoon505@gmail.com>
2023-05-16 22:20:03 +0900

    다익스트라

--- Baekjun/src/gold/B_1516_게임개발.java
+++ Baekjun/src/gold/B_1516_게임개발.java
@@ -1,59 +1,6 @@
 package gold;
-// import java.io.*;
-// import java.util.*;
-
-// public class B_1516_게임개발 {
-//     public static void main(String[] args) throws IOException{
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         int N = Integer.parseInt(br.readLine());
-//         ArrayList<Integer>[] arr = new ArrayList[N+1];
-//         for(int i=1;i<=N;i++){
-//             arr[i]  = new ArrayList<>();
-//         }
-//         int[] indegree = new int[N+1];
-//         int[] selfBuild = new int[N+1];
-//         // 그냥 입력 데이터를 전부 입력 받은거임.정렬 배열도 초기화 시키고 짖는 시간도 초기화 
-//         // 시키고 인접 리스트도 초기화 시키고
-//         for(int i=1;i<=N;i++){
-//             StringTokenizer st = new StringTokenizer(br.readLine()," ");
-//             selfBuild[i] = Integer.parseInt(st.nextToken());
-//             while(true){
-//                 int preTemp = Integer.parseInt(st.nextToken());
-//                 if(preTemp == -1) break;
-//                 arr[preTemp].add(i);
-//                 indegree[i]++;
-//             }
-//         }
-
-//         Queue<Integer> q = new LinkedList<>();
-//         // 0인 애들을 q에 집어넣는다
-//         for(int i=1;i<=N;i++){
-//             //0이다 = 그래프에서 연결된적이 없다 = 건물들의 최고 조상이다.
-//             if(indegree[i]==0){
-//                 q.offer(i);
-//             }
-//         }
-//         // 정답 배열 선언
-//         int[] result = new int[N+1];
-//         while(!q.isEmpty()){
-//             int now = q.poll();
-//             for(int i : arr[now]){
-//                 indegree[i]--;
-//                 result[i] = Math.max(result[i],result[now]+selfBuild[now]);
-//                 if(indegree[i]==0) q.add(i);
-//             }
-//         }
-//         StringBuffer sb  =new StringBuffer();
-//         for(int i=1;i<=N;i++){
-//             sb.append(result[i]+selfBuild[i]+"\n");
-//         }
-//         System.out.print(sb.toString());
-//     }
-// }
-
 import java.io.*;
 import java.util.*;
-
 public class B_1516_게임개발{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -62,38 +9,35 @@ public class B_1516_게임개발{
         for(int i=1;i<=N;i++){
             arr[i] = new ArrayList<>();
         }
-        int[] degree = new int[N+1];
         int[] selfBuild = new int[N+1];
+        int[] indegree = new int[N+1];
         for(int i=1;i<=N;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            StringTokenizer st  = new StringTokenizer(br.readLine()," ");
             selfBuild[i] = Integer.parseInt(st.nextToken());
             while(true){
-                int tmp = Integer.parseInt(st.nextToken());
-                if(tmp==-1) break;
-                arr[tmp].add(i);
-                degree[i]++;
-
+                int upBuild = Integer.parseInt(st.nextToken());
+                if(upBuild == -1) break;
+                arr[upBuild].add(i);
+                indegree[i]++;
             }
         }
         Queue<Integer> q = new LinkedList<>();
         for(int i=1;i<=N;i++){
-            // 0이다  = 최고조상이다
-            if(degree[i]==0) q.add(i);
+            if(indegree[i] == 0) q.offer(i);
         }
         int[] result = new int[N+1];
         while(!q.isEmpty()){
             int now = q.poll();
             for(int i : arr[now]){
-                degree[i]--;
+                indegree[i]--;
                 result[i] = Math.max(result[i],result[now]+selfBuild[now]);
-                if(degree[i]==0) q.add(i);
+                if(indegree[i] == 0) q.offer(i);
             }
         }
         StringBuffer sb = new StringBuffer();
-       for(int i=1;i<=N;i++){
-        sb.append(result[i]+selfBuild[i]+"\n");
-       }
+        for(int i=1;i<=N;i++){
+            sb.append(result[i] + selfBuild[i]+"\n");
+        }
         System.out.print(sb.toString());
-
     }
 }
--- Baekjun/src/gold/B_1753_최단경로.java
+++ Baekjun/src/gold/B_1753_최단경로.java
@@ -1,63 +1,58 @@
 package gold;
-import java.util.*;
 import java.io.*;
+import java.util.*;
 public class B_1753_최단경로{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int N  = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int start=  Integer.parseInt(br.readLine());
-        ArrayList<Node1753>[] arr = new ArrayList[N+1];
-        for(int i=1;i<=N;i++){
+        int node = Integer.parseInt(st.nextToken());
+        int edge = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine());
+        int[] distance = new int[node+1];
+        for(int i=1;i<=node;i++){
+            distance[i] = Integer.MAX_VALUE;
+        }
+        distance[start] = 0;
+        ArrayList<Node>[] arr = new ArrayList[node+1];
+        for(int i=1;i<=node;i++){
             arr[i] = new ArrayList<>();
         }
-        for(int i = 0;i<M;i++){
+        for(int i=0;i<edge;i++){
             st = new StringTokenizer(br.readLine()," ");
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            arr[s].add(new Node1753(e, v));
+            int value = Integer.parseInt(st.nextToken());
+            arr[s].add(new Node(e,value));
         }
-        PriorityQueue<Node1753> q = new PriorityQueue<>();
-        boolean[] visited = new boolean[N+1];
-        q.add(new Node1753(start,0));
-        int[] sol = new int[N+1];
-        for(int i=1;i<=N;i++){
-            sol[i] = Integer.MAX_VALUE;
-        }
-        sol[start]=0;
+        PriorityQueue<Node> q = new PriorityQueue<>((o1,o2)->{
+            return o1.value - o2.value;
+        });
+        q.add(new Node(start,0));
+        boolean[] visited = new boolean[node+1];
         while(!q.isEmpty()){
-            Node1753 now = q.poll();
-            if(visited[now.tnode]) continue;
-            visited[now.tnode] = true;
-            for(Node1753 i : arr[now.tnode]){
-                int tnode = i.tnode;
-                int value = i.value;
-                if(sol[tnode] > sol[now.tnode]+value){
-                    sol[tnode]=sol[now.tnode]+value;
-                    q.add(new Node1753(tnode, sol[tnode]));
+            Node now = q.poll();
+            if(!visited[now.node]){
+                visited[now.node] = true;
+                for(Node i : arr[now.node]){
+                   distance[i.node] = Math.min(distance[i.node],distance[now.node]+i.value);
+                   q.add(new Node(i.node,distance[i.node])); 
                 }
             }
         }
-        StringBuffer sb  =new StringBuffer();
-        for(int i=1;i<=N;i++){
-            if(visited[i]) sb.append(sol[i]+"\n");
-            else sb.append("INF\n");
+        StringBuffer sb = new StringBuffer();
+        for(int i=1;i<=node;i++){
+            if(distance[i] == Integer.MAX_VALUE) sb.append("INF\n");
+            else sb.append(distance[i]+"\n");
         }
         System.out.print(sb.toString());
 
     }
-}
-
-class Node1753 implements Comparable<Node1753>{
-    int tnode;
-    int value;
-    Node1753(int tnode,int value){
-        this.tnode = tnode;
-        this.value = value;
-    }
-    public int compareTo(Node1753 n){
-        return this.value - n.value;
+    static class Node{
+        int node;
+        int value;
+        Node(int node,int value){
+            this.node = node;
+            this.value = value;
+        }
     }
 }
