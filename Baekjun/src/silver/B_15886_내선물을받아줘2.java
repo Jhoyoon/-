ccfe1a3e0ed8a08cda9e3003281f4f4b6575948d
Jhoyoon <junghoyoon505@gmail.com>
2023-05-25 12:56:55 +0900

    확률 그래프를 배움

---
+++ Baekjun/src/silver/B_15886_내선물을받아줘2.java
@@ -0,0 +1,17 @@
+package silver;
+import java.io.*;
+import java.util.*;
+public class B_15886_내선물을받아줘2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[] c = br.readLine().toCharArray();
+        int count = 0;
+        for(int i=1;i<N;i++){
+            if(c[i-1] == 'E' && c[i] == 'W'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
