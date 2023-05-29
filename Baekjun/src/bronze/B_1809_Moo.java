17483032d3c1e013a9cd3febdb7b5bef6442fb8d
Jhoyoon <junghoyoon505@gmail.com>
2023-05-29 23:04:58 +0900

    스피카

---
+++ Baekjun/src/bronze/B_1809_Moo.java
@@ -0,0 +1,11 @@
+package bronze;
+
+public class B_1809_Moo {
+    public static void main(String[] args){
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
